47b13fe775f25018c76dc5bf58ea9a5f899ac41c
Gabriel Medina <gabi89.m@gmail.com>
2018-12-27 10:33:04 -0300

    Migrations for Teams and Players added

---
+++ database/migrations/2018_12_27_132935_create_players_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('team_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->timestamps();
+
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
